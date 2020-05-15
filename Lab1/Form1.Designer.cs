5a87dabe21e34456b131de63106bda9ecfd89442
ternovandarius <52597957+ternovandarius@users.noreply.github.com>
2020-05-15 15:31:05 +0300

    add lab 1

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.view = new System.Windows.Forms.DataGridView();
+            this.viewChildren = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.view)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewChildren)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // view
+            // 
+            this.view.AccessibleName = "";
+            this.view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.view.Location = new System.Drawing.Point(32, 24);
+            this.view.Name = "view";
+            this.view.RowHeadersWidth = 51;
+            this.view.RowTemplate.Height = 24;
+            this.view.Size = new System.Drawing.Size(785, 188);
+            this.view.TabIndex = 0;
+            // 
+            // viewChildren
+            // 
+            this.viewChildren.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewChildren.Location = new System.Drawing.Point(32, 307);
+            this.viewChildren.Name = "viewChildren";
+            this.viewChildren.RowHeadersWidth = 51;
+            this.viewChildren.RowTemplate.Height = 24;
+            this.viewChildren.Size = new System.Drawing.Size(785, 210);
+            this.viewChildren.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1109, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1109, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1109, 126);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1109, 154);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(963, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "EpisodeID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(963, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "EpisodeNR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(963, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "EpisodeTitle";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(963, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "DateAired";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(985, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(985, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(985, 340);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 545);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.viewChildren);
+            this.Controls.Add(this.view);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.view)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewChildren)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView view;
+        private System.Windows.Forms.DataGridView viewChildren;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Lab1
+{
+    //seasons is the parent table
+    //episodes is the child table
+    public partial class Form1 : Form
+    {
+        private BindingSource bsParent, bsChild;
+        private String connString = "Server=DESKTOP-944BR9T\\SQLEXPRESS;Database=Ternovan_Lab;Trusted_Connection=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getData()
+        {
+            SqlConnection dbConn = new SqlConnection(connString);
+            DataSet ds = new DataSet();
+
+            SqlDataAdapter daParent = new SqlDataAdapter("SELECT * FROM Seasons", dbConn);
+            SqlDataAdapter daChild = new SqlDataAdapter("select * from Episodes", dbConn);
+            daParent.Fill(ds, "Seasons");
+            daChild.Fill(ds, "Episodes");
+            DataRelation dr = new DataRelation("SeasonsEpisodes", ds.Tables["Seasons"].Columns["SeasonID"], ds.Tables["Episodes"].Columns["SeasonID"]);
+            ds.Relations.Add(dr);
+            bsParent = new BindingSource(ds, "Seasons");
+            bsChild = new BindingSource(bsParent, "SeasonsEpisodes");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(viewChildren.SelectedCells[0].Value.ToString());
+            SqlConnection dbConn = new SqlConnection(connString);
+            try
+            {
+                dbConn.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Episodes WHERE EpisodeID=@Id ", dbConn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                cmd.ExecuteNonQuery();
+                getData();
+                this.view.DataSource = bsParent;
+                this.viewChildren.DataSource = bsChild;
+            }
+            catch (SqlException s)
+            {
+                Console.WriteLine(s.Message);
+            }
+            finally
+            {
+                dbConn.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(textBox1.Text);
+            int nr = int.Parse(textBox2.Text);
+            String title = textBox3.Text;
+            String date = textBox4.Text;
+            int seasonID = int.Parse(view.SelectedCells[0].Value.ToString());
+
+            SqlConnection dbConn = new SqlConnection(connString);
+            try
+            {
+                dbConn.Open();
+                SqlCommand cmd = new SqlCommand("set identity_insert Episodes ON", dbConn);
+                cmd.ExecuteNonQuery();
+                SqlCommand comm = new SqlCommand("insert into Episodes (EpisodeID, EpisodeNr, EpisodeTitle, DateAired, SeasonID) values (@Id, @Nr, @Title, @Date, @SID)", dbConn);
+                comm.Parameters.AddWithValue("@Id", id);
+                comm.Parameters.AddWithValue("@Nr", nr);
+                comm.Parameters.AddWithValue("@Title", title);
+                comm.Parameters.AddWithValue("@Date", DateTime.Parse(date)); //date needs a YYYY-MM-DD  or DD-MM-YYYY format
+                comm.Parameters.AddWithValue("@SID", seasonID);
+                comm.ExecuteNonQuery();
+                getData();
+                this.view.DataSource = bsParent;
+                this.viewChildren.DataSource = bsChild;
+            }
+            catch (SqlException s)
+            {
+                Console.WriteLine(s.Message);
+            }
+            finally
+            {
+                dbConn.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(viewChildren.SelectedCells[0].Value.ToString());
+            try
+            {
+                int nr = int.Parse(textBox2.Text);
+                String title = textBox3.Text;
+                String date = textBox4.Text;
+                int seasonID = int.Parse(view.SelectedCells[0].Value.ToString());
+                SqlConnection dbConn = new SqlConnection(connString);
+                try
+                {
+                    dbConn.Open();
+                    SqlCommand comm = new SqlCommand("update Episodes set EpisodeNr=@Nr, EpisodeTitle=@Title, DateAired=@Date, SeasonID=@SID where EpisodeID = @Id", dbConn);
+                    comm.Parameters.AddWithValue("@Id", id);
+                    comm.Parameters.AddWithValue("@Nr", nr);
+                    comm.Parameters.AddWithValue("@Title", title);
+                    comm.Parameters.AddWithValue("@Date", DateTime.Parse(date));    //date needs a YYYY-MM-DD  or DD-MM-YYYY format
+                    comm.Parameters.AddWithValue("@SID", seasonID);
+                    comm.ExecuteNonQuery();
+                    getData();
+                    this.view.DataSource = bsParent;
+                    this.viewChildren.DataSource = bsChild;
+                }
+                catch (SqlException s)
+                {
+                    Console.WriteLine(s.Message);
+                }
+                finally
+                {
+                    dbConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            getData();
+            this.view.DataSource = bsParent;
+            this.viewChildren.DataSource = bsChild;
+        }
+    }
+}
+

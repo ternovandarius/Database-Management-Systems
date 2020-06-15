d67d0433db2760525d800943f48839a8697baf44
ternovandarius <52597957+ternovandarius@users.noreply.github.com>
2020-06-15 12:13:32 +0300

    Added assignment 2, as well as readme to both assignments

---
+++ Assignment 2/Lab2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.view = new System.Windows.Forms.DataGridView();
+            this.viewChildren = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.view)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewChildren)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // view
+            // 
+            this.view.AccessibleName = "";
+            this.view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.view.Location = new System.Drawing.Point(32, 24);
+            this.view.Name = "view";
+            this.view.RowHeadersWidth = 51;
+            this.view.RowTemplate.Height = 24;
+            this.view.Size = new System.Drawing.Size(785, 188);
+            this.view.TabIndex = 0;
+            // 
+            // viewChildren
+            // 
+            this.viewChildren.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewChildren.Location = new System.Drawing.Point(32, 307);
+            this.viewChildren.Name = "viewChildren";
+            this.viewChildren.RowHeadersWidth = 51;
+            this.viewChildren.RowTemplate.Height = 24;
+            this.viewChildren.Size = new System.Drawing.Size(785, 210);
+            this.viewChildren.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(985, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Update Table";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1255, 545);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.viewChildren);
+            this.Controls.Add(this.view);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.view)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.viewChildren)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView view;
+        private System.Windows.Forms.DataGridView viewChildren;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Assignment 2/Lab2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Configuration;
+
+namespace Lab2
+{
+    //seasons is the parent table
+    //episodes is the child table
+    public partial class Form1 : Form
+    {
+        private String parentName = ConfigurationManager.AppSettings["parentname"];
+        private String childName = ConfigurationManager.AppSettings["childname"];
+        private String selectParent = ConfigurationManager.AppSettings["selectparent"];
+        private String selectChild = ConfigurationManager.AppSettings["selectchild"];
+        private String foreignKeyName = ConfigurationManager.AppSettings["foreignkeyname"];
+        private String foreignKeyCol = ConfigurationManager.AppSettings["foreignkeycol"];
+
+        private SqlDataAdapter daParent, daChild;
+        private BindingSource bsParent, bsChild;
+        private SqlConnection dbConn = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        private DataSet ds;
+        private SqlCommandBuilder builder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            daChild.Update(ds.Tables[childName]);
+            daParent.Update(ds.Tables[parentName]);
+            getData();
+            this.view.DataSource = bsParent;
+            this.viewChildren.DataSource = bsChild;
+        }
+
+        private void getData()
+        {
+            ds = new DataSet();
+
+            daParent = new SqlDataAdapter(selectParent, dbConn);
+            daChild = new SqlDataAdapter(selectChild, dbConn);
+            daParent.Fill(ds, parentName);
+            daChild.Fill(ds, childName);
+
+            builder = new SqlCommandBuilder(daChild);
+
+            DataRelation dr = new DataRelation(foreignKeyName, ds.Tables[parentName].Columns[foreignKeyCol], ds.Tables[childName].Columns[foreignKeyCol]);
+            ds.Relations.Add(dr);
+
+            bsParent = new BindingSource(ds, parentName);
+            bsChild = new BindingSource(bsParent, foreignKeyName);
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            getData();
+            this.view.DataSource = bsParent;
+            this.viewChildren.DataSource = bsChild;
+        }
+    }
+}
+
